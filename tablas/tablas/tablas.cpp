2558d823777e56fc786d69734491f8b5d3a5987f
AndresYzimar CanoRios <acano4@ucol.mx>
2019-09-02 22:20:57 -0500

    sd

--- tablas/tablas/tablas.cpp
+++ tablas/tablas/tablas.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-	int resultado, num, mult;
+	int resultado, num, multiplicacion;
 	int i;
 	cout << "tablas de multiplicar" << endl ;
 	cout << "ingresa la tabla de multiplicar que quiere: ";
 	cin >> num;
 	cout << "asta que numero quiere que se multiplique:";
-	cin >> mult;
-	for ( i = 0; i < mult; i++)
+	cin >> multiplicacion;
+	for ( i = 0; i < multiplicacion; i++)
 	{
 		resultado = num * (i + 1);
 		cout << num << " * " << i + 1 << " = " << resultado << endl;
